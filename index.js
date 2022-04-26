a0925168f8f5e8e230957b22c2702fbf24151375
nyinyilayhtet <nnlayhtet@gmail.com>
2022-04-25 21:14:39 -0400

    commit tree 45fed8a15ceefb855a0ddb31dee9b5fb9b896eb9

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    if (someValue>42) return someValue-42;
+    else return 42-someValue
+}
+
+
+function distanceFromHqInFeet(someValue){
+    return 264*(distanceFromHqInBlocks(someValue))
+}
+
+
+function distanceTravelledInFeet(start, destination){
+    if (destination>start) return (destination-start)*264;
+    else return (start-destination)*264;
+}
+
+
+function calculatesFarePrice(start, destination){
+   let distance = distanceTravelledInFeet(start, destination);
+    if (distance >= 2500){
+        return 'cannot travel that far'
+    }
+    else if (distance >= 2000 && distance < 2500){
+        return 25;
+    }
+    else if (distance >= 400 && distance < 2000){
+        return (distance-400)*0.02;
+    }
+    else return 0;
+}
